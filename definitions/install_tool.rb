c53c6f76db551f6f560dc023358bcad79c36ddb7
Ed Overton <e.overton@gmail.com>
2018-04-04 14:48:32 -0400

    add definition

---
+++ definitions/install_tool.rb
@@ -0,0 +1,76 @@
+define :install_tool,
+       tool_pkg: '1',
+       tool_nam: '2',
+       tool_ver: '3',
+       tool_dir: '4',
+       tool_ins: '5',
+       tool_loc: '6' do
+  puts "tool package   = #{params[:tool_pkg]}"
+  puts "tool name      = #{params[:tool_nam]}"
+  puts "tool version   = #{params[:tool_ver]}"
+  puts "tool directory = #{params[:tool_dir]}"
+  puts "tool installed = #{params[:tool_ins]}"
+  puts "tool location  = #{params[:tool_loc]}"
+
+  # Create the dir's that are needed by netcool tool
+  directory node['nc_tools']['install_dir'] do
+    user node['nc_tools']['nc_act']
+    group node['nc_tools']['nc_grp']
+    recursive true
+    not_if { File.exist?(params[:tool_loc]) }
+    mode '0755'
+  end
+
+  # Download the tool package file
+  remote_file "#{node['nc_tools']['install_dir']}/#{params[:tool_pkg]}" do
+    source "#{node['nc_tools']['media_url']}/#{params[:tool_pkg]}"
+    not_if { File.exist?("#{node['nc_tools']['install_dir']}/#{params[:tool_pkg]}") }
+    not_if { File.exist?(params[:tool_ins]) }
+    not_if { File.exist?(params[:tool_loc]) }
+    user node['nc_tools']['nc_act']
+    group node['nc_tools']['nc_grp']
+    mode '0755'
+    action :create
+  end
+
+  # unzip the tool package file
+  execute 'unzip_package' do
+    command "unzip -q #{node['nc_tools']['install_dir']}/#{params[:tool_pkg]}"
+    cwd node['nc_tools']['install_dir']
+    not_if { File.exist?(params[:tool_ins]) }
+    not_if { File.exist?(params[:tool_loc]) }
+    user node['nc_tools']['nc_act']
+    group node['nc_tools']['nc_grp']
+    umask '022'
+    action :run
+  end
+
+  template "#{node['nc_tools']['temp_dir']}/install_nc_tool.xml" do
+    source 'install_nc_tool.xml.erb'
+    not_if { File.exist?(params[:tool_loc]) }
+    mode 0755
+  end
+
+  execute 'install_tool' do
+    command "#{node['nc_tools']['app_dir']}/InstallationManager/eclipse/tools/imcl \
+    input #{node['nc_tools']['temp_dir']}/install_nc_tool.xml \
+    -log #{node['nc_tools']['temp_dir']}/install-nc_tool_log.xml \
+    -acceptLicense"
+    not_if { File.exist?(params[:tool_loc]) }
+    user node['nc_tools']['nc_act']
+    group node['nc_tools']['nc_grp']
+    umask '022'
+    action :run
+  end
+
+  file "#{node['nc_tools']['temp_dir']}/install_nc_tool.xml" do
+    only_if { File.exist?(params[:tool_loc]) }
+    action :delete
+  end
+
+  directory node['nc_tools']['install_dir'] do
+    only_if { File.exist?(params[:tool_loc]) }
+    recursive true
+    action :delete
+  end
+end
--- recipes/install_gw_remedy.rb
+++ recipes/install_gw_remedy.rb
@@ -1,14 +1,9 @@
-# Override generic variables
-node.default['nc_tools']['tool_name'] = 'nco-g-bmc-remedy'
-node.default['nc_tools']['tool_dir']  = node['nc_tools']['install_dir']
-node.default['nc_tools']['tool_ver']  = '1.3.0.3'
-node.default['nc_tools']['package']   = 'Im-nco-g-bmc-remedy-3_0.zip'
-node.default['nc_tools']['tool_inst'] = "#{node.default['nc_tools']['tool_dir']}/install.txt"
-node.default['nc_tools']['tool_loc']  = "#{node['nc_tools']['ob_dir']}/gates/bmc_remedy/default/bmc_remedy.map"
-execute 'running_remedy' do
-  command "echo #{node['nc_tools']['tool_loc']} #{node['nc_tools']['tool_name']} "
+# Install the Netcool tool bmc_remedy
+install_tool 'Remedy' do
+  tool_pkg 'Im-nco-g-bmc-remedy-3_0.zip'
+  tool_nam 'nco-g-bmc-remedy'
+  tool_ver '1.3.0.3'
+  tool_dir node['nc_tools']['install_dir']
+  tool_ins "#{node.default['nc_tools']['tool_dir']}/install.txt"
+  tool_loc "#{node['nc_tools']['ob_dir']}/gates/bmc_remedy/default/bmc_remedy.map"
 end
-# Install Base Netcool
-include_recipe 'nc_base::make_nc_base'
-# Install the Netcool tool
-include_recipe '::install_nc_tool'
