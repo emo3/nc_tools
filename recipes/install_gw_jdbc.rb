ff446d20960410432c65ffcf98d4962f3d2545cd
Ed Overton <e.overton@gmail.com>
2018-04-04 13:37:41 -0400

    setup remedy

--- recipes/install_gw_jdbc.rb
+++ recipes/install_gw_jdbc.rb
@@ -3,8 +3,11 @@ node.default['nc_tools']['tool_name'] = 'nco-g-jdbc'
 node.default['nc_tools']['tool_dir']  = node['nc_tools']['install_dir']
 node.default['nc_tools']['tool_ver']  = '1.6.0.4'
 node.default['nc_tools']['package']   = 'CN4FUEN.zip'
-node.default['nc_tools']['tool_inst'] = "#{node['nc_tools']['tool_dir']}/install.txt"
+node.default['nc_tools']['tool_inst'] = "#{node.default['nc_tools']['tool_dir']}/install.txt"
 node.default['nc_tools']['tool_loc']  = "#{node['nc_tools']['ob_dir']}/gates/jdbc/default/jdbc.map"
+execute 'running_jdbc' do
+  command "echo #{node['nc_tools']['tool_loc']} #{node['nc_tools']['tool_name']} "
+end
 # Install Base Netcool
 include_recipe 'nc_base::make_nc_base'
 # Install the Netcool tool
--- recipes/install_gw_remedy.rb
+++ recipes/install_gw_remedy.rb
@@ -3,8 +3,11 @@ node.default['nc_tools']['tool_name'] = 'nco-g-bmc-remedy'
 node.default['nc_tools']['tool_dir']  = node['nc_tools']['install_dir']
 node.default['nc_tools']['tool_ver']  = '1.3.0.3'
 node.default['nc_tools']['package']   = 'Im-nco-g-bmc-remedy-3_0.zip'
-node.default['nc_tools']['tool_inst'] = "#{node['nc_tools']['tool_dir']}/install.txt"
+node.default['nc_tools']['tool_inst'] = "#{node.default['nc_tools']['tool_dir']}/install.txt"
 node.default['nc_tools']['tool_loc']  = "#{node['nc_tools']['ob_dir']}/gates/bmc_remedy/default/bmc_remedy.map"
+execute 'running_remedy' do
+  command "echo #{node['nc_tools']['tool_loc']} #{node['nc_tools']['tool_name']} "
+end
 # Install Base Netcool
 include_recipe 'nc_base::make_nc_base'
 # Install the Netcool tool
