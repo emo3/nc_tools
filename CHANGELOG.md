c0f92132eb822185d006916f00206cc1d27eb301
Ed Overton <e.overton@gmail.com>
2019-02-26 20:52:08 -0500

    fix copyright, etc

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# nc_tools CHANGELOG
+
+This file is used to list changes made in each version of the nc_tools cookbook.
+
+# 0.1.0
+
+Initial release.
+
+- change 0
+- change 1
--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,8 @@ license 'Apache 2.0'
 description 'Installs/Configures nc_tools'
 long_description 'Installs/Configures nc_tools'
 version '0.1.0'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+chef_version '>= 13.0'
+supports 'redhat'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
@@ -18,6 +19,5 @@ chef_version '>= 12.14' if respond_to?(:chef_version)
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/nc_tools'
-depends 'delivery-truck'
 depends 'nc_base', '~> 0.1.0'
 depends 'server_utils', '~> 0.1.0'
