320fddefebd2281c7ad654341566c7d68bafd772
Ed Overton <e.overton@gmail.com>
2020-05-17 21:38:34 -0400

    bump version

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name 'nc_tools'
 maintainer 'Ed Overton'
-maintainer_email 'infuse.1301@gmail.com'
+maintainer_email 'bogus@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures nc_tools'
 long_description 'Installs/Configures nc_tools'
-version '0.3.2'
+version '0.3.3'
 chef_version '>= 13.0'
 supports 'redhat'
 
